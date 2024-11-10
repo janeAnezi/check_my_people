5bf1ae86145c65ad4e2b457c1514d7c585814f82
Anezijane <ahnnieanezi@gmail.com>
2024-11-11 00:27:13 +0100

    add images for platform slide

---
+++ src/Components/PlatformSlide.jsx
@@ -0,0 +1,19 @@
+import img1 from '../assets/logo01.png'
+import img2 from '../assets/logo02.png'
+import img3 from '../assets/logo03.png'
+import img4 from '../assets/logo04.png'
+import img5 from '../assets/logo05.png'
+import img6 from '../assets/logo06.png'
+
+export default function PlatformSlide() {
+  return (
+    <div className='logo-slide'>
+      <img src={img1} alt="" />
+      <img src={img2} alt="" />
+      <img src={img3} alt="" />
+      <img src={img4} alt="" />
+      <img src={img5} alt="" />
+      <img src={img6} alt="" />
+    </div>
+  )
+}
