092d66f676d054b84213e72d1538d7a19edf9b63
Anezijane <ahnnieanezi@gmail.com>
2024-11-10 22:33:30 +0100

    Add nav bar content

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+      <img className='logo' src="" alt="" />
+      <ul>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">Resource</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">Our Services</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">Company</a></li>
+        <li><a href="http://" target="_blank" rel="noopener noreferrer">Mobile App</a></li>
+      </ul>
+    </div>
+  )
+}
