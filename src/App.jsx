0e4c4c26218a1b178838954168318b06d90d61aa
Anezijane <ahnnieanezi@gmail.com>
2024-11-11 00:51:38 +0100

    Add platform content

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,9 @@ function App() {
   return (
     <>
       <NavBar />
-      <Home />
+      <Home heading='Confirming your staff identity and credibility' par='An all in one platform to help you verify & confirm that employees are who they say they are.' btnOne='Get Started' btnTwo='Login' />
       <Platform />
+      <Home heading='Verify your Child Nanny with CheckMyPeople' par='People have a need for trust, security, and peace of mind within their homes. They seek assurance that the individuals they hire are trustworthy, reliable, and pose no threat to their safety or the safety of their families.' btnOne='Get Started' btnTwo='Login'/>
     </>
   )
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,14 @@
 import homeImage from '../assets/home.png'
 
-export default function Home() {
+export default function Home({heading, par, btnOne, btnTwo}) {
   return (
     <div className='home'>
         <div className='left'>
-            <h1>Confirming your staff identity and credibility</h1>
-            <p>An all in one platform to help you verify & confirm that employees are who they say they are.</p>
+            <h1>{heading}</h1>
+            <p>{par}</p>
             <div>
-                <button className='get-started-btn'>Get Started</button>
-                <button className='login-btn'>Login</button>
+                <button className='get-started-btn'>{btnOne} </button>
+                <button className='login-btn'>{btnTwo}</button>
             </div>
         </div>
         <div><img src={homeImage} alt="a hand with directional arrow" /></div>
