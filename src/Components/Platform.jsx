3f90453e267b9829f2e5e90b43f92b086d243d36
Anezijane <ahnnieanezi@gmail.com>
2024-11-11 00:32:28 +0100

    add images for platform slide

--- src/Components/Platform.jsx
+++ src/Components/Platform.jsx
@@ -1,27 +1,31 @@
 import Imag01 from '../assets/cardimag01.png'
 import Card from './Card'
+import PlatformSlide from './PlatformSlide'
 
 export default function Platform() {
   return (
-    <div className='platform'>
-      <p className='par'>An all in one platform to help you verify & confirm that employees are who they say they are.</p>
-      <div className='platform-cards'>
-        <Card title='01'> 
-            <img src={Imag01} alt="Person verification status" />
-            <h2>Verify Identity</h2>
-            <p>We offer our service via our web platform as well as Online App (CheckMyPeople) on Play Store and App Store.</p>
-        </Card>
-        <Card title='02'>
-            <img src={Imag01} alt="Person verification status" />
-            <h2>Verify Identity</h2>
-            <p>We offer our service via our web platform as well as Online App (CheckMyPeople) on Play Store and App Store.</p>
-        </Card>
-        <Card title='03'>
-            <img src={Imag01} alt="Person verification status" />
-            <h2>Verify Identity</h2>
-            <p>We offer our service via our web platform as well as Online App (CheckMyPeople) on Play Store and App Store.</p>
-        </Card>
-      </div>
+    <div>
+        <div className='platform'>
+        <p className='par'>An all in one platform to help you verify & confirm that employees are who they say they are.</p>
+        <div className='platform-cards'>
+            <Card title='01'> 
+                <img src={Imag01} alt="Person verification status" />
+                <h2>Verify Identity</h2>
+                <p>We offer our service via our web platform as well as Online App (CheckMyPeople) on Play Store and App Store.</p>
+            </Card>
+            <Card title='02'>
+                <img src={Imag01} alt="Person verification status" />
+                <h2>Verify Identity</h2>
+                <p>We offer our service via our web platform as well as Online App (CheckMyPeople) on Play Store and App Store.</p>
+            </Card>
+            <Card title='03'>
+                <img src={Imag01} alt="Person verification status" />
+                <h2>Verify Identity</h2>
+                <p>We offer our service via our web platform as well as Online App (CheckMyPeople) on Play Store and App Store.</p>
+            </Card>
+        </div>
+        </div>
+        <PlatformSlide />
     </div>
   )
 }
--- src/Components/PlatformSlide.jsx
+++ src/Components/PlatformSlide.jsx
@@ -1,6 +1,6 @@
-import img1 from '../assets/logo01.png'
-import img2 from '../assets/logo02.png'
-import img3 from '../assets/logo03.png'
+import img1 from '../assets/logo02.png'
+import img2 from '../assets/logo03.png'
+import img3 from '../assets/logo07.png'
 import img4 from '../assets/logo04.png'
 import img5 from '../assets/logo05.png'
 import img6 from '../assets/logo06.png'
@@ -8,12 +8,12 @@ import img6 from '../assets/logo06.png'
 export default function PlatformSlide() {
   return (
     <div className='logo-slide'>
-      <img src={img1} alt="" />
-      <img src={img2} alt="" />
-      <img src={img3} alt="" />
-      <img src={img4} alt="" />
-      <img src={img5} alt="" />
-      <img src={img6} alt="" />
+      <img src={img1} alt="company logo" />
+      <img src={img2} alt="company logo" />
+      <img src={img3} alt="company logo" />
+      <img src={img4} alt="company logo" />
+      <img src={img5} alt="company logo" />
+      <img src={img6} alt="company logo" />
     </div>
   )
 }
